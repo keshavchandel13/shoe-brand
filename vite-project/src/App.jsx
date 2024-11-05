1c088df2389a8d42e4b6db0dd8576fc212652a6d
Keshav Chandel <chandelkeshav4@gmail.com>
2024-11-05 15:40:26 +0530

    fixed error in app.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from'./components/Navbar'
+import MainPage from "./components/MainPage.jsx";
 function App() {
   return (
     <>
     <Navbar/>
+    <MainPage />
     </>
   )
 }
